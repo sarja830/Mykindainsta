31f479b3b70a80a3a8ab1928bdc00ebd271a0012
sarja830 <sarthjain830@gmail.com>
2020-07-10 22:47:05 +0530

    added custom email support removed sendergrid api

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,8 @@ module.exports={
     JWT_SECRET:,
       SENDGRID_API:,
     EMAIL:"http:localhost:3000"
-
+    NODE_MAILER_EMAIL:"sender email",
+    PASSWORD:"sender password"
 
 
 
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,9 @@
 module.exports={
-    MONGOURI:process.env.MONGOURI,
-    JWT_SECRET:process.env.JWT_SEC,
+    MONGOURI:"can be found at mongo db atlas",
+    JWT_SECRET:"",
+    SENDGRID_API:"Not required if not using sendgrid",
+    EMAIL:"http:localhost:3000",
+     NODE_MAILER_EMAIL:"sender email",
+    PASSWORD:"sender password"
+   
 }
